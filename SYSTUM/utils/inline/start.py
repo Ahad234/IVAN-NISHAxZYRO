9c6a195154d8300525665a31fe595646106c51a1
IVANxDEVELOPER <96462140+Ahad234@users.noreply.github.com>
2025-09-04 19:47:18 +0530

    Update start.py

--- SYSTUM/utils/inline/start.py
+++ SYSTUM/utils/inline/start.py
@@ -1,8 +1,6 @@
 from pyrogram.types import InlineKeyboardButton
-
 import config
 from SYSTUM import app
-
 
 def start_panel(_):
     buttons = [
@@ -15,7 +13,6 @@ def start_panel(_):
     ]
     return buttons
 
-
 def private_panel(_):
     buttons = [
         [
@@ -24,14 +21,17 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/SilentlyFuck"),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_5"], url="https://t.me/x9Ahad"  # Replace with actual username
+            )
         ],
     ]
     return buttons
