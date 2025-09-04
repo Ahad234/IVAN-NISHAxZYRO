0d2702180a06c057bb4d0c9db232ee704e9e84f5
IVANxDEVELOPER <96462140+Ahad234@users.noreply.github.com>
2025-09-04 19:41:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ REPO_LINK = getenv("REPO_LINK" , "https://github.com/Ahad234/IVAN-NISHAxZYRO")
 BOT_NAME = getenv("BOT_NAME" , ".")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://ahaan:ahaad@ahaan.hgkeruq.mongodb.net/?retryWrites=true&w=majority&appName=ahaan")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://shreyan82683:shreyan82683@cluster0.c8v26ub.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
